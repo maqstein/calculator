06fc5e689695fbcd3594ac74173f2f848b46bcb0
maqstein <maqstein@gmail.com>
2019-04-14 23:51:11 +0200

    Update main.py
    123321

--- main.py
+++ main.py
@@ -15,13 +15,14 @@ Config.set('graphics', 'width', 300)
 
 class MyApp(App):
 
-    def calculate(self, instance):
-
+    def calculate(self):
         try:
             self.formula = str(eval(self.label.text))
         except ZeroDivisionError:
-            self.formula="ошибка"
+            self.formula="opening a black hole"
+            # TODO: open a black hole here
         self.update_label()
+
     def clear_label(self, instance):
         self.formula = '0'
         self.update_label()
@@ -38,6 +39,7 @@ class MyApp(App):
 
     def build(self):
 
+        # some crutch here
         self.label = Label(text='0', font_size=40, halign='right', text_size=(295, 100), size_hint=(1, 0.4))
 
         self.formula = '0'
@@ -45,35 +47,39 @@ class MyApp(App):
         grid_layout = GridLayout(rows=5, cols=4)
         box_layout.add_widget(self.label)
 
-        grid_layout.add_widget(Button(text='%', on_press=self.add_a_thing))
-        grid_layout.add_widget(Button(text='/', on_press=self.add_a_thing))
-        grid_layout.add_widget(Button(text='*', on_press=self.add_a_thing))
-        grid_layout.add_widget(Button(text='-', on_press=self.add_a_thing))
+        # magic
 
-        grid_layout.add_widget(Button(text='7', on_press=self.add_a_thing))
-        grid_layout.add_widget(Button(text='8', on_press=self.add_a_thing))
-        grid_layout.add_widget(Button(text='9', on_press=self.add_a_thing))
-        grid_layout.add_widget(Button(text='+', on_press=self.add_a_thing))
-
-        grid_layout.add_widget(Button(text='4', on_press=self.add_a_thing))
-        grid_layout.add_widget(Button(text='5', on_press=self.add_a_thing))
-        grid_layout.add_widget(Button(text='6', on_press=self.add_a_thing))
-        grid_layout.add_widget(Button(text='Clr', on_press=self.clear_label))
-
-        grid_layout.add_widget(Button(text='1', on_press=self.add_a_thing))
-        grid_layout.add_widget(Button(text='2', on_press=self.add_a_thing))
-        grid_layout.add_widget(Button(text='3', on_press=self.add_a_thing))
-        grid_layout.add_widget(Button(text='=', on_press=self.calculate))
-
-
-        grid_layout.add_widget(Button(text=''))
-        grid_layout.add_widget(Button(text='0', on_press=self.add_a_thing))
-        grid_layout.add_widget(Button(text='.', on_press=self.add_a_thing))
-        grid_layout.add_widget(Button(text=''))
+        for i in self.operations:
+            if not bool(self.operations[i]): # if there is no value in dictionary this will be False(cause of not it will be True)
+                grid_layout.add_widget(Button(text=f"{i}",on_press=self.add_a_thing))
+            else:
+                grid_layout.add_widget(Button(text=f"{i}",on_press=self.operations[i]))
 
         box_layout.add_widget(grid_layout)
         return box_layout
 
+    operations ={
+                '%':'',
+                '/':'',
+                '*':'',
+                '-':'',
+                '7':'',
+                '8':'',
+                '9':'',
+                '+':'',
+                '4':'',
+                '5':'',
+                '6':'',
+                'Clr':clear_label,
+                '1':'',
+                '2':'',
+                '3':'',
+                '=':calculate,
+                '':'',
+                '0':'',
+                '.':'',
+                '':'',
+                }
 
 if __name__ == "__main__":
     MyApp().run()
