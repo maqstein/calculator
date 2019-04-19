0a0e986d0796892ed019cfc32198ba733e4fbf5c
maqstein <maqstein@gmail.com>
2019-04-19 15:40:03 +0200

    my first calculator

--- main.py
+++ main.py
@@ -14,72 +14,79 @@ Config.set('graphics', 'width', 300)
 
 
 class MyApp(App):
-
-    def calculate(self):
+    calculation_result = None
+    def calculate(self,instance):
         try:
-            self.formula = str(eval(self.label.text))
+            self.calculation_result = eval(self.label_down.text)
+            self.formula = str(self.calculation_result)
         except ZeroDivisionError:
-            self.formula="opening a black hole"
-            # TODO: open a black hole here
-        self.update_label()
+            self.formula="ошибка"
+        self.update_label_down()
 
-    def clear_label(self, instance):
+    def clear_label_down(self, instance):
         self.formula = '0'
-        self.update_label()
+        self.update_label_down()
 
-    def update_label(self):
-        self.label.text = self.formula
+    def update_label_down(self):
+        self.label_down.text = self.formula
+
+    def update_label_top(self):
+        self.label_top.text = str(self.calculation_result)
+        self.formula = ""
+        self.update_label_down()
 
     def add_a_thing(self, instance):
         if self.formula == "0":
             self.formula = ""
+        if (instance.text in self.operators) and not (self.formula is "") and (self.formula[-1] in self.operators):
 
+            self.update_label_down()
+            return
+        if self.formula == str(self.calculation_result):
+            self.update_label_top()
         self.formula += str(instance.text)
-        self.update_label()
+        self.update_label_down()
 
     def build(self):
+        self.label_top = Label(text='', font_size=40, halign='right', text_size=(295, 50), size_hint=(1, 0.2))
+        self.label_down = Label(text='0', font_size=40, halign='right', text_size=(295, 50), size_hint=(1, 0.2))
 
-        # some crutch here
-        self.label = Label(text='0', font_size=40, halign='right', text_size=(295, 100), size_hint=(1, 0.4))
 
-        self.formula = '0'
         box_layout = BoxLayout(orientation='vertical')
         grid_layout = GridLayout(rows=5, cols=4)
-        box_layout.add_widget(self.label)
+        box_layout.add_widget(self.label_top)
+        box_layout.add_widget(self.label_down)
 
-        # magic
 
-        for i in self.operations:
-            if not bool(self.operations[i]): # if there is no value in dictionary this will be False(cause of not it will be True)
-                grid_layout.add_widget(Button(text=f"{i}",on_press=self.add_a_thing))
-            else:
-                grid_layout.add_widget(Button(text=f"{i}",on_press=self.operations[i]))
+        grid_layout.add_widget(Button(text='%', on_press=self.add_a_thing))
+        grid_layout.add_widget(Button(text='/', on_press=self.add_a_thing))
+        grid_layout.add_widget(Button(text='*', on_press=self.add_a_thing))
+        grid_layout.add_widget(Button(text='-', on_press=self.add_a_thing))
+
+        grid_layout.add_widget(Button(text='7', on_press=self.add_a_thing))
+        grid_layout.add_widget(Button(text='8', on_press=self.add_a_thing))
+        grid_layout.add_widget(Button(text='9', on_press=self.add_a_thing))
+        grid_layout.add_widget(Button(text='+', on_press=self.add_a_thing))
+
+        grid_layout.add_widget(Button(text='4', on_press=self.add_a_thing))
+        grid_layout.add_widget(Button(text='5', on_press=self.add_a_thing))
+        grid_layout.add_widget(Button(text='6', on_press=self.add_a_thing))
+        grid_layout.add_widget(Button(text='Clr', on_press=self.clear_label_down))
+
+        grid_layout.add_widget(Button(text='1', on_press=self.add_a_thing))
+        grid_layout.add_widget(Button(text='2', on_press=self.add_a_thing))
+        grid_layout.add_widget(Button(text='3', on_press=self.add_a_thing))
+        grid_layout.add_widget(Button(text='=', on_press=self.calculate))
+
+
+        grid_layout.add_widget(Button(text=''))
+        grid_layout.add_widget(Button(text='0', on_press=self.add_a_thing))
+        grid_layout.add_widget(Button(text='.', on_press=self.add_a_thing))
+        grid_layout.add_widget(Button(text=''))
 
         box_layout.add_widget(grid_layout)
         return box_layout
-
-    operations ={
-                '%':'',
-                '/':'',
-                '*':'',
-                '-':'',
-                '7':'',
-                '8':'',
-                '9':'',
-                '+':'',
-                '4':'',
-                '5':'',
-                '6':'',
-                'Clr':clear_label,
-                '1':'',
-                '2':'',
-                '3':'',
-                '=':calculate,
-                '':'',
-                '0':'',
-                '.':'',
-                '':'',
-                }
-
+    operators = "+-/%*"
+    formula = '0'
 if __name__ == "__main__":
     MyApp().run()
